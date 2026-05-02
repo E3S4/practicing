7db0e1edffe6ef8f6ee29195cd463ecd2b370165
E3S4 <eesak490@gmail.com>
2026-05-02 15:03:36 +0530

    now changing value of variable using pointer :)

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int x = 10;
-    int *p = &x;
-
-    printf("%d\n", x);   // 10
+int x = 10;
+int *p = &x;
+// pointer changer idk value changes of var
+*p = 50;
+    printf("%d\n", x);   
     printf("%p\n", p);   // address of x
-    printf("%d\n", *p);  // 10
+    printf("%d\n", *p);  
 
     return 0;
 }
